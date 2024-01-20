c34ce10cf091d455d3fee2aa737cfb318500d97c
nullniverse <nullniverse@pm.me>
2024-01-20 12:17:49 -0300

    naming adjustments

--- helpers/consts.js
+++ helpers/consts.js
@@ -11,7 +11,7 @@ export const GRAFITI_ARRAY = GRAFFITI.split('\n');
 export const INTRO_TEXT = [
   ...GRAFITI_ARRAY,
   'Welcome to JumentOS!',
-  'The JumentoSEC Operational System.',
+  'This is the Jumento Security Conferences Operational System.',
   '',
   '',
   'If it\'s your first access, respect our "policy".',
